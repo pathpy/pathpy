1b10d96a960af59135f86aff925b8f7af78f71ce
Jürgen Hackl <hackl@protonmail.com>
2019-09-09 16:41:50 +0200

    add testing environment

---
+++ pathpy/tests/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+# =============================================================================
+# File      : __init__.py -- init file for pathpy testing
+# Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
+# Time-stamp: <Mon 2019-09-09 08:44 juergen>
+#
+# Copyright (c) 2016-2019 Pathpy Developers
+# =============================================================================
+
+
+# =============================================================================
+# eof
+#
+# Local Variables:
+# mode: python
+# mode: linum
+# mode: auto-fill
+# fill-column: 79
+# End:
