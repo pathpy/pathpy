9125da206ec8008429924dc31d47fcf9129456e4
Jürgen Hackl <hackl@protonmail.com>
2019-10-15 12:27:42 +0200

    add directory for subpath methods

---
+++ pathpy/subpaths/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+# =============================================================================
+# File      : __init__.py -- Initialize methods for the subpath analysis
+# Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
+# Time-stamp: <Tue 2019-10-15 11:25 juergen>
+#
+# Copyright (c) 2016-2019 Pathpy Developers
+# =============================================================================
+
+
+# =============================================================================
+# eof
+#
+# Local Variables:
+# mode: python
+# mode: linum
+# mode: auto-fill
+# fill-column: 79
+# End:
