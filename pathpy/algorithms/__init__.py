20f4a067ecdc5f4f91d025a0919c199062d0cee4
Jürgen Hackl <hackl@protonmail.com>
2020-03-25 14:34:01 +0100

    fix git conflict

--- pathpy/algorithms/__init__.py
+++ pathpy/algorithms/__init__.py
@@ -3,17 +3,14 @@
 # =============================================================================
 # File      : __init__.py -- Initialize network and path algorithms
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Wed 2020-03-25 12:31 juergen>
+# Time-stamp: <Wed 2020-03-25 14:33 juergen>
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
 
-<<<<<<< HEAD
 from . import shortest_paths
 from . import centralities
 
-=======
->>>>>>> 8f1a209... initial short term bug fix for #2
 # =============================================================================
 # eof
 #
