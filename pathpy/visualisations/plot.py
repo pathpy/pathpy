c0f5d9455df0dbe4be762bd8be74de1358758260
Jürgen Hackl <hackl@protonmail.com>
2020-04-27 07:47:20 +0200

    changed time stamp

--- pathpy/visualisations/plot.py
+++ pathpy/visualisations/plot.py
@@ -4,7 +4,7 @@
 # =============================================================================
 # File      : plot.py -- Module to plot pathoy networks
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Wed 2020-04-22 19:51 juergen>
+# Time-stamp: <Wed 2020-04-22 21:58 juergen>
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
