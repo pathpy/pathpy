9d1960b51955cd2ff55ff637bb152cdab3f84620
Jürgen Hackl <hackl@protonmail.com>
2020-05-14 16:11:28 +0200

    fix type checking

--- pathpy/algorithms/trees.py
+++ pathpy/algorithms/trees.py
@@ -4,12 +4,12 @@
 # =============================================================================
 # File      : tree.py -- Module containing tree algorithms
 # Author    : Ingo Scholtes <scholtes@uni-wuppertal.de>
-# Time-stamp: <Mon 2020-05-11 19:33 ingo>
+# Time-stamp: <Thu 2020-05-14 16:10 juergen>
 #
 # Copyright (c) 2016-2020 Pathpy Developers
 # =============================================================================
 from __future__ import annotations
-from typing import Any, List, Union, Optional
+from typing import TYPE_CHECKING, Any, List, Union, Optional
 from functools import singledispatch
 from collections import defaultdict
 
@@ -17,23 +17,23 @@ import numpy as np
 from scipy import sparse  # pylint: disable=import-error
 
 from pathpy import logger
-from pathpy.core.base import BaseNetwork
+
+# pseudo load class for type checking
+if TYPE_CHECKING:
+    from pathpy.core.network import Network
 
 # create logger
 LOG = logger(__name__)
 
 
+def check_tree(network: Network):
 
+    if network.directed:
 
-def check_tree(network: BaseNetwork):
-
-    
-    if network.directed: 
-        
         # identify node with zero indegree
         root = None
         for v in network.nodes.uids:
-            if network.indegrees()[v]==0:
+            if network.indegrees()[v] == 0:
                 if root == None:
                     root = v
                 else:   # two nodes with in-degree zero -> no tree
@@ -43,8 +43,8 @@ def check_tree(network: BaseNetwork):
 
         visited = defaultdict(bool)
 
-        def dfs(network: BaseNetwork, node: str):
-            
+        def dfs(network: Network, node: str):
+
             nonlocal visited
 
             visited[node] = True
@@ -56,14 +56,14 @@ def check_tree(network: BaseNetwork):
                     tree &= dfs(network, v.uid)
             return tree
         return dfs(network, root)
-    
-    else: 
+
+    else:
         LOG.error('Tree checking not supported for undirected networks')
-    
+
     return False
 
 
-def tree_size(network: BaseNetwork, node: str):
+def tree_size(network: Network, node: str):
 
     size = 1
     for v in network.successors[node]:
