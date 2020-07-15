f30cbc9301b18f72ffa1d0fe8863010a0a3366f4
Jürgen Hackl <hackl@protonmail.com>
2020-07-15 17:55:42 +0200

    add higher-order network class to pathpy

--- pathpy/models/higher_order_network.py
+++ pathpy/models/higher_order_network.py
@@ -4,17 +4,19 @@
 # =============================================================================
 # File      : higher_order_network.py -- Basic class for a HON
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Mon 2020-06-29 17:51 juergen>
+# Time-stamp: <Wed 2020-07-15 17:54 juergen>
 #
 # Copyright (c) 2016-2020 Pathpy Developers
 # =============================================================================
 
 from __future__ import annotations
-from typing import Any, Optional, Union, Generic
+from typing import Any, Optional, Union
+from itertools import islice
+from singledispatchmethod import singledispatchmethod
 
 from pathpy import logger
 from pathpy.core.node import Node, NodeCollection
-from pathpy.core.edge import Edge, EdgeCollection
+from pathpy.core.edge import Edge, EdgeCollection, EdgeSet
 from pathpy.core.path import Path, PathCollection
 from pathpy.core.network import Network
 
@@ -23,141 +25,161 @@ from pathpy.models.models import ABCHigherOrderNetwork
 # create logger for the Network class
 LOG = logger(__name__)
 
-"""Questions about HONs
 
-How to deal with the 0-order start node?
+class HigherOrderNetwork(ABCHigherOrderNetwork, Network):
+    """Base class for a Higher Order Network (HON)."""
 
-How is the order of a node determined?
-Can this be done on a node level or
-must it be done on the network level?
+    def __init__(self, uid: Optional[str] = None, order: int = 1,
+                 **kwargs: Any) -> None:
+        """Initialize the higer-order network object."""
 
-How to get nodes and edges?
+        # initialize the base class
+        super().__init__(uid=uid, directed=True, temporal=False,
+                         multiedges=False, **kwargs)
 
-Forced uids like in pathpy2 ? a and b -> ('a,b')
+        # order of the higher-order network
+        self._order: int = order
 
-Nodes
-hon.nodes[abc] # hon-node object
-hon.nodes['abc'] # hon-node uid
-hon.nodes[a,b,c] # node objects
-hon.nodes['a','b','c'] # node uids
-hon.nodes[ab,bc] # edge objects
-hon.nodes['a-b','b-c'] # edge uids
+        # a container for node objects
+        self._nodes: Any = HigherOrderNodeCollection()
 
-Edges
-hon.edges[abcd] # hon-edge object
-hon.edges['abcd'] # hon-edge uid
-hon.edges[abc,bcd] # hon-node objects
-hon.edges['abc','bcd'] # hon-node uids
-hon.edges[('a','b','c'),('b','c','d')] # node uids
-hon.edges[('a-b','b-c'),('b-c','c-d')] # edge uids
-hon.edges['a','b','c','d'] # path node uids
-hon.edges['a-b','b-c','c-d'] # path edge uids
-hon.edges[a,b,c,d] # node objects
-hon.edges[ab,bc,cd] # edge objects
+        # a container for edge objects
+        self._edges: Any = HigherOrderEdgeCollection(nodes=self._nodes)
 
-Methods
+    @property
+    def order(self) -> int:
+        """Return the order of the higher-order network."""
+        return self._order
 
-How to implement:
+    def degrees_of_freedom(self, mode: str = 'path') -> int:
+        """Returns the degrees of freedom of the higher-order network.
 
-estimator = estimator.fit(data, targets)
-estimator = estimator.fit(data)
+        Since probabilities must sum to one, the effective degree of freedom is
+        one less than the number of nodes
 
-prediction = predictor.predict(data)
-probability = predictor.predict_proba(data)
+        .. math::
 
-new_data = transformer.transform(data)
-new_data = transformer.fit_transform(data)
+           \\text{dof} = \\sum_{n \\in N} \\max(0,\\text{outdeg}(n)-1)
 
-score = model.score(data)
+        """
+        # initialize degree of freedom
+        degrees_of_freedom: int = 0
+        return degrees_of_freedom
 
+    def summary(self) -> str:
+        """Returns a summary of the higher-order network.
 
+        The summary contains the name, the used network class, the order, the
+        number of nodes and edges.
 
-Model.from_samples(data)
+        If logging is enabled (see config), the summary is written to the log
+        file and showed as information on in the terminal. If logging is not
+        enabled, the function will return a string with the information, which
+        can be printed to the console.
 
-"""
+        Returns
+        -------
+        str
+            Returns a summary of important higher-order network properties.
 
+        """
+        summary = [
+            'Uid:\t\t\t{}\n'.format(self.uid),
+            'Type:\t\t\t{}\n'.format(self.__class__.__name__),
+            # 'Directed:\t\t{}\n'.format(str(self.directed)),
+            # 'Multi-Edges:\t\t{}\n'.format(str(self.multiedges)),
+            'Order:\t\t\t{}\n'.format(self.order),
+            'Number of nodes:\t{}\n'.format(self.number_of_nodes()),
+            'Number of edges:\t{}'.format(self.number_of_edges()),
+        ]
+        attr = self.attributes.to_dict()
+        if len(attr) > 0:
+            summary.append('\n\nNetwork attributes\n')
+            summary.append('------------------\n')
+        for k, v in attr.items():
+            summary.append('{}:\t{}\n'.format(k, v))
 
-# class HigherOrderNetwork(ABCHigherOrderNetwork, Network):
-#     """Base class for a Higher Order Network (HON)."""
+        return ''.join(summary)
 
-# def __init__(self, uid: Optional[str] = None, order: int = 1,
-#              **kwargs: Any) -> None:
-#     """Initialize the higer-order network object."""
+    @singledispatchmethod
+    def fit(self, data, order: int = 1, subpaths: bool = True) -> None:
+        """Fit data to a HigherOrderNetwork"""
+        raise NotImplementedError
 
-#     # initialize the base class
-#     super().__init__(uid=uid, directed=True, temporal=False,
-#                      multiedges=False, **kwargs)
+    @fit.register(PathCollection)
+    def _(self, data: PathCollection, order: int = 1, subpaths: bool = True) -> None:
+        print("I'm a PathCollection object")
 
-#     # order of the higher-order network
-#     self._order: int = order
+        # TODO: create function to transfer base data from PathCollection object
+        # --- START ---
+        nc = NodeCollection()
+        for node in data.nodes.values():
+            nc.add(node)
 
-#     # a container for node objects
-#     self._nodes: HigherOrderNodeCollection = HigherOrderNodeCollection()
+        ec = EdgeCollection(nodes=nc)
+        for edge in data.edges.values():
+            ec.add(edge)
 
-#     # a container for edge objects
-#     self._edges: HigherOrderEdgeCollection = HigherOrderEdgeCollection(
-#         directed=True,
-#         multiedges=False,
-#         nodes=self._nodes)
+        self._nodes = HigherOrderNodeCollection(nodes=nc, edges=ec)
+        # --- END ---
 
-# def add_node(self, *node: Union[str, Node, HigherOrderNode], **kwargs: Any) -> None:
-#     """Add a single higher-order node to the network."""
-#     self.nodes.add(*node, **kwargs)
+        # iterate over all paths
+        for path in data:
 
-# @property
-# def order(self) -> int:
-#     """Return the order of the higher-order network."""
-#     return self._order
+            # get frequency of the observed path
+            # TODO: define keyword in config file
+            frequency = path.attributes.get('frequency', 1)
 
-# def degrees_of_freedom(self, mode: str = 'path') -> int:
-#     """Returns the degrees of freedom of the higher-order network.
+            nodes: list = []
+            if order == 0:
+                for node in path.nodes:
+                    if (node,) not in self.nodes:
+                        self.nodes.add(node)
+            elif order == 1:
+                nodes.extend([tuple([n]) for n in path.nodes])
 
-#     Since probabilities must sum to one, the effective degree of freedom is
-#     one less than the number of nodes
+            elif 1 < order <= len(path):
+                for subpath in self.window(path.edges, size=order-1):
+                    nodes.append(subpath)
 
-#     .. math::
+            elif order == len(path)+1:
+                if tuple(path.edges) not in self.nodes:
+                    self.nodes.add(tuple(path.edges))
 
-#        \\text{dof} = \\sum_{n \\in N} \\max(0,\\text{outdeg}(n)-1)
+            else:
+                pass
 
-#     """
-#     # initialize degree of freedom
-#     degrees_of_freedom: int = 0
-#     return degrees_of_freedom
+            _edges = set()
+            for _v, _w in zip(nodes[:-1], nodes[1:]):
 
-# def summary(self) -> str:
-#     """Returns a summary of the higher-order network.
+                if _v not in self.nodes:
+                    self.nodes.add(_v)
 
-#     The summary contains the name, the used network class, the order, the
-#     number of nodes and edges.
+                if _w not in self.nodes:
+                    self.nodes.add(_w)
 
-#     If logging is enabled (see config), the summary is written to the log
-#     file and showed as information on in the terminal. If logging is not
-#     enabled, the function will return a string with the information, which
-#     can be printed to the console.
+                _nodes = (self.nodes[_v], self.nodes[_w])
+                if _nodes not in self.edges:
+                    self.edges.add(*_nodes, frequency=0, observed=0)
 
-#     Returns
-#     -------
-#     str
-#         Returns a summary of important higher-order network properties.
+                _edges.add(self.edges[_nodes])
 
-#     """
-#     summary = [
-#         'Uid:\t\t\t{}\n'.format(self.uid),
-#         'Type:\t\t\t{}\n'.format(self.__class__.__name__),
-#         # 'Directed:\t\t{}\n'.format(str(self.directed)),
-#         # 'Multi-Edges:\t\t{}\n'.format(str(self.multiedges)),
-#         'Order:\t\t\t{}\n'.format(self.order),
-#         'Number of nodes:\t{}\n'.format(self.number_of_nodes()),
-#         'Number of edges:\t{}'.format(self.number_of_edges()),
-#     ]
-#     attr = self.attributes.to_dict()
-#     if len(attr) > 0:
-#         summary.append('\n\nNetwork attributes\n')
-#         summary.append('------------------\n')
-#     for k, v in attr.items():
-#         summary.append('{}:\t{}\n'.format(k, v))
+            for edge in _edges:
+                edge['frequency'] += frequency
+                if order == len(path):
+                    edge['observed'] += frequency
 
-#     return ''.join(summary)
+    @staticmethod
+    def window(iterable, size=2):
+        """Sliding window for path length"""
+        ite = iter(iterable)
+        result = tuple(islice(ite, size))
+        if len(result) == size:
+            yield result
+        for elem in ite:
+            result = result[1:] + (elem,)
+            yield result
+
 
 class HigherOrderNode(Node, Path):
     """Base class of a higher order node."""
@@ -168,6 +190,8 @@ class HigherOrderNode(Node, Path):
         # initializing the parent classes
         Node.__init__(self, uid, **kwargs)
         Path.__init__(self, *args, uid=uid, **kwargs)
+
+        self['label'] = '-'.join([n.uid for n in self.nodes])
 
     @property
     def order(self) -> int:
@@ -205,14 +229,18 @@ class HigherOrderNode(Node, Path):
 class HigherOrderNodeCollection(PathCollection):
     """Higher-order node collection."""
 
-    def __init__(self) -> None:
+    def __init__(self, nodes=None, edges=None) -> None:
         """Initialize the NodeCollection object."""
 
         # initialize the base class
-        super().__init__()
+        super().__init__(nodes=nodes, edges=edges)
 
         # class of objects
         self._path_class = HigherOrderNode
+
+    def _if_exist(self, path: Any, **kwargs: Any) -> None:
+        """If the node already exists"""
+        print('dddddddddd')
 
 
 class HigherOrderEdge(Edge):
@@ -224,8 +252,84 @@ class HigherOrderEdge(Edge):
         super().__init__(v=v, w=w, uid=uid, **kwargs)
 
 
-# class HigherOrderEdgeCollection(EdgeCollection):
-#     pass
+class HigherOrderEdgeCollection(EdgeCollection):
+    """Higher-order node collection."""
+
+    def __init__(self, nodes: Optional[HigherOrderNodeCollection] = None) -> None:
+        """Initialize the HigherOrderEdgeCollection object."""
+
+        # initialize the base class
+        super().__init__()
+
+        self._nodes = HigherOrderNodeCollection()
+        if nodes is not None:
+            self._nodes = nodes
+
+        # class of objects
+        self._edge_class = HigherOrderEdge
+
+    def __getitem__(self,
+                    key: Union[str, tuple, Edge]) -> Union[Edge, EdgeSet, EdgeCollection]:
+        """Returns a node object."""
+
+        if isinstance(key, tuple):
+            _node = tuple(self.nodes[i] for i in key)
+            if self.multiedges:
+                edge = self._nodes_map[_node]
+            else:
+                edge = self._nodes_map[_node][-1]
+
+        elif isinstance(key, self._edge_class) and key in self:
+            edge = key
+        else:
+            edge = self._map[key]
+        return edge
+
+    @singledispatchmethod
+    def __contains__(self, item) -> bool:
+        """Returns if item is in edges."""
+        return super().__contains__(item)
+
+    @__contains__.register(tuple)  # type: ignore
+    @__contains__.register(list)
+    def _(self, item: Union[tuple, list]) -> bool:
+        """Returns if item is in edges."""
+        _contain: bool = False
+
+        if all([isinstance(i, (str, Node)) for i in item]):
+            try:
+                if tuple(self.nodes[i] for i in item) in self._nodes_map:
+                    _contain = True
+            except KeyError:
+                pass
+        elif all([isinstance(i, tuple) for i in item]):
+            try:
+                if self._nodes_map[(self.nodes[item[0]],
+                                    self.nodes[item[1]])] is not None:
+                    _contain = True
+            except KeyError:
+                pass
+
+        return _contain
+
+    @singledispatchmethod
+    def add(self, *edge, **kwargs: Any) -> None:
+        """Add multiple edges. """
+
+        raise NotImplementedError
+
+    @add.register(HigherOrderEdge)  # type: ignore
+    def _(self, *edge: HigherOrderEdge, **kwargs: Any) -> None:
+        super().add(*edge, **kwargs)
+
+    @add.register(HigherOrderNode)  # type: ignore
+    def _(self, *edge: HigherOrderNode, **kwargs: Any) -> None:
+        super().add(*edge, **kwargs)
+
+    @add.register(str)  # type: ignore
+    def _(self, *edge: str, **kwargs: Any) -> None:
+        super().add(*edge, **kwargs)
+
 
 # =============================================================================
 # eof
--- pathpy/tests/test_higher_order_network.py
+++ pathpy/tests/test_higher_order_network.py
@@ -3,7 +3,7 @@
 # =============================================================================
 # File      : test_higher_order_network.py -- Test environment for HONs
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Mon 2020-06-29 18:19 juergen>
+# Time-stamp: <Wed 2020-07-15 17:46 juergen>
 #
 # Copyright (c) 2016-2020 Pathpy Developers
 # =============================================================================
@@ -14,7 +14,10 @@ from pathpy.models.higher_order_network import (
     HigherOrderNode,
     HigherOrderEdge,
     HigherOrderNodeCollection,
+    HigherOrderEdgeCollection,
+    HigherOrderNetwork,
 )
+from pathpy.core.path import PathCollection
 # #                                                 # HigherOrderEdge
 # #                                                 )
 # from pathpy.core.path import HigherOrderNode
@@ -41,6 +44,13 @@ def test_higher_order_node():
     assert nodes[ab, bc] == abc
     assert nodes['a-b', 'b-c'] == abc
 
+    assert abc in nodes
+    assert 'abc' in nodes
+    assert (a, b, c) in nodes
+    assert ('a', 'b', 'c') in nodes
+    assert (ab, bc) in nodes
+    assert ('a-b', 'b-c') in nodes
+
 
 def test_higher_order_edge():
     """Test higher order edges."""
@@ -50,28 +60,100 @@ def test_higher_order_edge():
     c = Node('c', color='cyan')
     d = Node('d', color='desert')
 
-    ab = Edge(a, b, uid='ab')
-    bc = Edge(b, c, uid='bc')
-    cd = Edge(c, d, uid='cd')
+    ab = Edge(a, b, uid='a-b')
+    bc = Edge(b, c, uid='b-c')
+    cd = Edge(c, d, uid='c-d')
 
     abc = HigherOrderNode(ab, bc, uid='abc')
     bcd = HigherOrderNode(bc, cd, uid='bcd')
 
     abc_bcd = HigherOrderEdge(abc, bcd, uid='abc-bcd')
-    # print(abc_bcd)
 
 
-# def test_higher_order_network():
-#     """Default test for development"""
-#     hon = HigherOrderNetwork()
-#     hon.add_node('a', uid='a')
-#     hon.add_node('b', uid='b')
-#     hon.add_edge('a', 'b', uid='a-b')
+def test_higher_order_edge_collection():
+    """Test HigherOrderEdgeCollection."""
+
+    a = Node('a', color='azure')
+    b = Node('b', color='blue')
+    c = Node('c', color='cyan')
+    d = Node('d', color='desert')
+    e = Node('e', color='desert')
+
+    ab = Edge(a, b, uid='a-b')
+    bc = Edge(b, c, uid='b-c')
+    cd = Edge(c, d, uid='c-d')
+    de = Edge(d, e, uid='d-e')
+
+    abc = HigherOrderNode(ab, bc, uid='abc')
+    bcd = HigherOrderNode(bc, cd, uid='bcd')
+    cde = HigherOrderNode(cd, de, uid='cde')
+
+    abc_bcd = HigherOrderEdge(abc, bcd, uid='abc-bcd')
+    bcd_cde = HigherOrderEdge(bcd, cde, uid='bcd-cde')
+
+    edges = HigherOrderEdgeCollection()
+
+    edges.add(abc_bcd)
+
+    assert edges[abc_bcd] == abc_bcd
+    assert edges['abc-bcd'] == abc_bcd
+    assert edges[abc, bcd] == abc_bcd
+    assert edges['abc', 'bcd'] == abc_bcd
+    assert edges[(a, b, c), (b, c, d)] == abc_bcd
+    assert edges[('a', 'b', 'c'), ('b', 'c', 'd')] == abc_bcd
+    assert edges[(ab, bc), (bc, cd)] == abc_bcd
+    assert edges[('a-b', 'b-c'), ('b-c', 'c-d')] == abc_bcd
+
+    # NotImplemented
+    # edges['a','b','c','d'] # path node uids
+    # edges['a-b','b-c','c-d'] # path edge uids
+    # edges[a,b,c,d] # node objects
+    # edges[ab,bc,cd] # edge objects
+
+    assert abc_bcd in edges
+    assert 'abc-bcd' in edges
+    assert (abc, bcd) in edges
+    assert ('abc', 'bcd') in edges
+    assert ((a, b, c), (b, c, d)) in edges
+    assert (('a', 'b', 'c'), ('b', 'c', 'd')) in edges
+    assert ((ab, bc), (bc, cd)) in edges
+    assert (('a-b', 'b-c'), ('b-c', 'c-d')) in edges
+
+
+def test_higher_order_network():
+    """Default test for development"""
+    hon = HigherOrderNetwork()
+
+    hon.add_node('a', 'b', 'c', uid='abc')
+    hon.add_node('b', 'c', 'd', uid='bcd')
+    hon.add_edge('abc', 'bcd', uid='abc-bcd')
 #     print(hon.nodes)
 #     #hon.add_edge('a', 'b', uid='e1')
-#     # print(hon)
-#     # print(hon.nodes)
-#     # print(hon.edges)
+    # print(hon)
+    # print(len(hon.nodes.edges))
+    # print(hon.edges)
+
+
+def test_fit_path_collection():
+    """Fit PathCollection to a HON"""
+    paths = PathCollection()
+    paths.add('a', 'c', 'd', uid='acd')
+    paths.add('b', 'c', 'e', uid='bce', frequency=5)
+    # paths.add('a', 'c', 'x', 'f', uid='acdf', frequency=7)
+    # paths.add('a', 'c', uid='ac', frequency=10)
+
+    hon = HigherOrderNetwork()
+    hon.fit(paths, order=2)
+
+    # print(len(hon.edges))
+    # for e in hon.edges:
+    #     print(e.uid, e['frequency'], e['observed'])
+    # # print(len(hon.nodes))
+    # print('nodes')
+    # for n in hon.nodes:
+    #     print(n.uid, n.nodes)
+
+    # hon.plot(filename='hontest.html')
 
 
 # =============================================================================
