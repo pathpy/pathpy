864985df6fc70f5ab0b764a12db61bc4855d2414
Jürgen Hackl <hackl@protonmail.com>
2020-09-25 10:06:23 +0200

    remove all __init__ files to focus on the core module

--- pathpy/__init__.py
+++ pathpy/__init__.py
@@ -4,7 +4,7 @@
 # =============================================================================
 # File      : __init__.py -- pathpy init file
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Thu 2020-09-17 08:33 juergen>
+# Time-stamp: <Fri 2020-09-25 10:05 juergen>
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
@@ -22,50 +22,50 @@ from pathpy.utils.progress import tqdm  # noqa: F401
 
 
 # import symbols into root namespace
-from pathpy.core.api import (Node,
-                             Edge,
-                             HyperEdge,
-                             Path,
-                             PathCollection,
-                             Network,
-                             )
+from pathpy.core.api import (Node,)
+# Edge,
+#                              HyperEdge,
+#                              Path,
+#                              PathCollection,
+#                              Network,
+#                              )
 
 
-from pathpy.models.api import (TemporalNetwork,
-                               DirectedAcyclicGraph,
-                               HigherOrderNetwork,
-                               NullModel,
-                               MultiOrderModel
-                               )
+# from pathpy.models.api import (TemporalNetwork,
+#                                DirectedAcyclicGraph,
+#                                HigherOrderNetwork,
+#                                NullModel,
+#                                MultiOrderModel
+#                                )
 
-from pathpy.visualisations.api import (plot,
-                                       layout)
+# from pathpy.visualisations.api import (plot,
+#                                        layout)
 
 # import models
 # from pathpy.models.api import (NullModel,
 #                                MultiOrderModel)
-from pathpy.models.api import (MOGen)
+# from pathpy.models.api import (MOGen)
 
 
 # import submodules
-from pathpy import io
-from pathpy import converters
-from pathpy import algorithms
-from pathpy import statistics
-from pathpy import processes
-from pathpy import generators
+# from pathpy import io
+# from pathpy import converters
+# from pathpy import algorithms
+# from pathpy import statistics
+# from pathpy import processes
+# from pathpy import generators
 
 
-from .__about__ import (
-    __title__,
-    __version__,
-    __author__,
-    __email__,
-    __copyright__,
-    __license__,
-    __maintainer__,
-    __status__
-)
+# from .__about__ import (
+#     __title__,
+#     __version__,
+#     __author__,
+#     __email__,
+#     __copyright__,
+#     __license__,
+#     __maintainer__,
+#     __status__
+# )
 
 # create logger for the the init file
 LOG = logger(__name__)
@@ -100,33 +100,33 @@ LOG.debug('pathpy version %s', __version__)
 LOG.debug('platform is %s', sys.platform)
 LOG.debug('pathpy runs in a %s environment', config['environment']['IDE'])
 
-if config['environment']['IDE'] == 'vs code':
-    _html = """
-    <script charset="utf-8">
-    // Load via requireJS if available (jupyter notebook environment)
-    try {
-    require.config({
-    paths: {
-    d3: "https://d3js.org/d3.v5.min.js".replace(".js", "")
-    }
-    });
-    console.log("OKAY: requireJS was detected");
-    }
-    catch(err){
-    console.log(err);
-    console.log("ERROR: NO requireJS was detected");
-    };
-    require(['d3'], function(d3){
-    console.log("OKAY: d3js was detected");
-    });
-    </script>
-    """
+# if config['environment']['IDE'] == 'vs code':
+#     _html = """
+#     <script charset="utf-8">
+#     // Load via requireJS if available (jupyter notebook environment)
+#     try {
+#     require.config({
+#     paths: {
+#     d3: "https://d3js.org/d3.v5.min.js".replace(".js", "")
+#     }
+#     });
+#     console.log("OKAY: requireJS was detected");
+#     }
+#     catch(err){
+#     console.log(err);
+#     console.log("ERROR: NO requireJS was detected");
+#     };
+#     require(['d3'], function(d3){
+#     console.log("OKAY: d3js was detected");
+#     });
+#     </script>
+#     """
 
-    try:
-        from IPython.display import display, HTML
-        display(HTML(_html))
-    except ImportError:
-        pass
+#     try:
+#         from IPython.display import display, HTML
+#         display(HTML(_html))
+#     except ImportError:
+#         pass
 # =============================================================================
 # eof
 #
