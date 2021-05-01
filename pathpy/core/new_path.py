260e1c02731930b97059188f2e89bd98412d3bc0
Jürgen Hackl <hackl@protonmail.com>
2021-05-01 17:33:42 +0800

    add BasePath object

--- pathpy/core/new_path.py
+++ pathpy/core/new_path.py
@@ -4,7 +4,7 @@
 # =============================================================================
 # File      : network.py -- Base class for a path
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Sat 2021-05-01 17:30 juergen>
+# Time-stamp: <Sat 2021-05-01 17:32 juergen>
 #
 # Copyright (c) 2016-2021 Pathpy Developers
 # =============================================================================
@@ -17,6 +17,9 @@ from singledispatchmethod import singledispatchmethod  # NOTE: not needed at 3.9
 
 from pathpy import logger
 from pathpy.core.classes import PathPyObject
+
+# create logger for the Path class
+LOG = logger(__name__)
 
 
 class PathTuple(tuple):
@@ -43,6 +46,156 @@ class PathTuple(tuple):
     def __repr__(self):
         return super().__repr__() if self.directed else '|'+super().__repr__()[1:-1]+'|'
 
+
+class BasePath(PathPyObject):
+    """Base class for a path."""
+
+    def __init__(self, *args: Union[str, PathPyObject],
+                 uid: Optional[str] = None,
+                 directed: bool = True,
+                 **kwargs: Any) -> None:
+        """Initialize the path object."""
+
+        # initialize the parent class
+        super().__init__(uid=uid, **kwargs)
+
+        # variable to indicate if path is directed or not
+        self._directed: bool = directed
+
+        # a storage containing structure of the objects
+        self._relations: PathTuple
+
+        # map to the associated objects
+        self._objects: dict = dict()
+
+        # if checking is disabled create path directly from args of str
+        if not kwargs.pop('checking', True):
+            self._relations = PathTuple(args, directed=directed)
+            self._objects = {uid: None for uid in args}
+            return
+
+        # helper variable for path assignment
+        _uids = []
+
+        # iterate over args and create structure and map
+        for arg in args:
+            # check if arg is a str (i.e. an uid)
+            if isinstance(arg, str):
+                _uid = arg
+                _obj = None
+
+            # check if arg is already a pathpy opject
+            elif isinstance(arg, PathPyObject):
+                _uid = arg.uid
+                _obj = arg
+
+            # if not applicable raise an error
+            else:
+                LOG.error('All objects must be str or PathPyObject s!')
+                raise TypeError
+
+            # temporal storage of the uids
+            _uids.append(_uid)
+
+            # create object mapping
+            if _uid not in self._objects and _uid != self.uid:
+                self._objects[_uid] = _obj
+
+            # if arg uid and self.uid is equal make a self reference
+            elif _uid not in self._objects and _uid == self.uid:
+                self._objects[_uid] = self
+
+        # save relationships
+        self._relations = PathTuple(_uids, directed=directed)
+
+    def __len__(self) -> int:
+        """Lenght of the object."""
+        return len(self.relations) - 1 if self.relations else 0
+
+    def __str__(self) -> str:
+        """Print a summary of the object. """
+        return self.summary()
+
+    def __contains__(self, item) -> bool:
+        """Returns if suppath is in path"""
+        return NotImplementedError
+
+    @property
+    def objects(self) -> dict:
+        """Return the associated objects. """
+        return self._objects
+
+    @property
+    def relations(self) -> PathTuple:
+        """Return the associated relations of the path. """
+        return self._relations
+
+    @property
+    def depth(self) -> int:
+        """Depth of the object."""
+        return self.max_depth(self)
+
+    @property
+    def directed(self) -> bool:
+        """Returns if the object is directed or not."""
+        return self._directed
+
+    @directed.setter
+    def directed(self, directed: bool) -> None:
+        """Set the direction of the path"""
+        self._directed = directed
+        self._relations.directed = directed
+
+    @staticmethod
+    def max_depth(item) -> int:
+        """Calculate the max depth of the object."""
+        # Filter valid objects
+        objects = _get_valid_objects(item)
+        # if no valid objects are given return 1
+        if not objects:
+            return 1
+        # recursive function  to get the depth
+        return 1 + max(BasePath.max_depth(child) for child in objects)
+
+    def subobjects(self, depth=None) -> list:
+        """Returns the sub objects at a certain depth."""
+        objects = _get_valid_objects(self)
+        result = [[self.relations]]
+        queue = objects
+
+        while queue:
+            result.append([node.nodes for node in queue])
+            if len(result) == depth and depth:
+                break
+
+            level = []
+            for node in queue:
+                level.extend(_get_valid_objects(node))
+
+            queue = level
+
+        if depth:
+            if depth <= len(result):
+                result = result[depth-1]
+            else:
+                result = []
+        return result
+
+    def summary(self) -> str:
+        """Returns a summary of the object. """
+
+        summary = [
+            'Uid:\t\t{}\n'.format(self.uid),
+            'Type:\t\t{}\n'.format(self.__class__.__name__),
+        ]
+
+        return ''.join(summary)
+
+
+def _get_valid_objects(item) -> list:
+    """Helper function to return a list with valid objects"""
+    return [obj for obj in item.objects.values()
+            if (isinstance(obj, BasePath) and obj.uid != item.uid)]
 
 # =============================================================================
 # eof
