5620b9a22aec9b11a6948ed6291de908d87e7e69
Ingo Scholtes <scholtes@uni-wuppertal.de>
2021-05-06 09:21:45 +0200

    adding unit tests

--- pathpy/tests/test_path_extraction.py
+++ pathpy/tests/test_path_extraction.py
@@ -22,14 +22,42 @@ def tempnet():
     tn.add_edge('c', 'd', timestamp=5)
     return tn
 
+@pytest.fixture
+def dag():
+    dag = pp.DirectedAcyclicGraph()
+    dag.add_edge('a', 'b')
+    dag.add_edge('b', 'c')
+    dag.add_edge('b', 'd')
+    dag.add_edge('c', 'e')
+    return dag
+
 pathdata = [
-    (1, Counter({('a', 'b', 'c'): 1, ('b', 'd'): 1} )),
-    (2, Counter({('a', 'b', 'c'): 1, ('b', 'd'): 1} )),
+    (1, Counter({('a', 'b', 'c'): 1, ('b', 'd'): 1, ('c', 'd'): 1} )),
+    (2, Counter({('a', 'b', 'c'): 1, ('b', 'd'): 1, ('c', 'd'): 1} )),
     (3, Counter({('a', 'b', 'c', 'd'): 1, ('b', 'd'): 1} )),
     (4, Counter({('a', 'b', 'c', 'd'): 1, ('a', 'b', 'd'): 1} ))
 ]
 
+dagdata = [
+    (1, [('a_1', 'b_2'), ('b_2', 'c_3'), ('b_5', 'd_6'), ('c_5', 'd_6')]),
+    (2, [('a_1', 'b_2'), ('a_1', 'b_3'), ('b_2', 'c_3'), ('b_2', 'c_4'), ('b_5', 'd_6'), ('b_5', 'd_7'), ('c_5', 'd_6'), ('c_5', 'd_7')]),
+    (3, [('a_1', 'b_2'), ('a_1', 'b_3'), ('b_2', 'c_3'), ('b_2', 'c_4'), ('b_5', 'd_6'), ('b_5', 'd_7'), ('c_5', 'd_6'), ('c_5', 'd_7'), ('a_1', 'b_4')]),
+    (4, [('a_1', 'b_2'), ('a_1', 'b_3'), ('b_2', 'c_3'), ('b_2', 'c_4'), ('b_5', 'd_6'), ('b_5', 'd_7'), ('c_5', 'd_6'), ('c_5', 'd_7'), ('a_1', 'b_4'), ('a_1', 'b_5')])
+]
+
 @pytest.mark.parametrize("delta,path_counts", pathdata)
-def test_path_extraction(tempnet, delta, path_counts):
+def test_path_extraction_temporal_network(tempnet, delta, path_counts):
     paths = pp.algorithms.path_extraction.all_paths_from_temporal_network(tempnet, delta=delta)
     assert paths == path_counts
+
+
+def test_path_extraction_dag(dag):
+    paths = pp.algorithms.path_extraction.all_paths_from_dag(dag)
+    assert paths == Counter({('a', 'b', 'c', 'e'): 1, ('a', 'b', 'd'): 1})
+
+
+@pytest.mark.parametrize("delta,expected_edges", dagdata)
+def test_temporal_net_to_dag(tempnet, delta, expected_edges):
+    dag = pp.DirectedAcyclicGraph.from_temporal_network(tempnet, delta=delta)
+    for e in expected_edges:
+        assert e in dag.edges
