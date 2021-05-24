d7dfc5d707602810ee3df58209bb689d43188af2
Jürgen Hackl <hackl@protonmail.com>
2021-05-24 12:46:03 +0800

    start the implementation of the new hon

---
+++ pathpy/models/new_higher_order_network.py
@@ -0,0 +1,25 @@
+"""Higher-order network class"""
+# !/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+# =============================================================================
+# File      : higher_order_network.py -- Basic class for a HON
+# Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
+# Time-stamp: <Mon 2021-05-24 12:45 juergen>
+#
+# Copyright (c) 2016-2020 Pathpy Developers
+# =============================================================================
+from pathpy import logger
+
+# create logger for the Network class
+LOG = logger(__name__)
+
+
+# =============================================================================
+# eof
+#
+# Local Variables:
+# mode: python
+# mode: linum
+# mode: auto-fill
+# fill-column: 79
+# End:
