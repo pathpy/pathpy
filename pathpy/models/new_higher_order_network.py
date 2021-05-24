e6ad6444545a2fce0c3c670a7502c588e4cbc0f0
Jürgen Hackl <hackl@protonmail.com>
2021-05-24 12:56:14 +0800

    add higher-order nodes and edge classes

--- pathpy/models/new_higher_order_network.py
+++ pathpy/models/new_higher_order_network.py
@@ -4,14 +4,30 @@
 # =============================================================================
 # File      : higher_order_network.py -- Basic class for a HON
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Mon 2021-05-24 12:45 juergen>
+# Time-stamp: <Mon 2021-05-24 12:55 juergen>
 #
 # Copyright (c) 2016-2020 Pathpy Developers
 # =============================================================================
-from pathpy import logger
+from typing import Any, Optional, Union
 
+from pathpy import logger
+from pathpy.core import PathPyPath
+from pathpy.core.edge import Edge
 # create logger for the Network class
 LOG = logger(__name__)
+
+
+class HigherOrderNode(PathPyPath):
+    """Base class of a higher-order node."""
+
+    @property
+    def order(self):
+        "Return the order of the higher-oder node"
+        return len(self)
+
+
+class HigherOrderEdge(Edge):
+    """Base class of an higher-order edge."""
 
 
 # =============================================================================
