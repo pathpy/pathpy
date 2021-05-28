4bfa944d7af11b46785690150ac37be1c1b84d21
Jürgen Hackl <hackl@protonmail.com>
2021-05-28 14:43:24 +0800

    start adapting the null model to the new pathpy api

--- pathpy/models/null_model.py
+++ pathpy/models/null_model.py
@@ -4,7 +4,7 @@
 # =============================================================================
 # File      : null_models.py -- Null models for pathpy
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Wed 2021-03-31 11:18 juergen>
+# Time-stamp: <Fri 2021-05-28 14:43 juergen>
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
@@ -38,218 +38,218 @@ class NullModel(HigherOrderNetwork):
         # initialize the base class
         super().__init__(uid=uid, order=order, **kwargs)
 
-    @singledispatchmethod
-    def fit(self, data, order: Optional[int] = None) -> None:
-        """Fit data to a NullModel"""
-        raise NotImplementedError
+    # @singledispatchmethod
+    # def fit(self, data, order: Optional[int] = None) -> None:
+    #     """Fit data to a NullModel"""
+    #     raise NotImplementedError
 
-    @fit.register(PathCollection)  # type: ignore
-    def _(self, data: PathCollection, order: Optional[int] = None) -> None:
+    # @fit.register(PathCollection)  # type: ignore
+    # def _(self, data: PathCollection, order: Optional[int] = None) -> None:
 
-        # Check order
-        if order is not None:
-            self._order = order
+    #     # Check order
+    #     if order is not None:
+    #         self._order = order
 
-        if 0 <= self.order <= 1:
-            super().fit(data, order=self.order)
+    #     if 0 <= self.order <= 1:
+    #         super().fit(data, order=self.order)
 
-        elif self.order > 1:
-            # --- START ---
-            nc = NodeCollection()
-            for node in data.nodes.values():
-                nc.add(node)
+    #     elif self.order > 1:
+    #         # --- START ---
+    #         nc = NodeCollection()
+    #         for node in data.nodes.values():
+    #             nc.add(node)
 
-            ec = EdgeCollection(nodes=nc)
-            for edge in data.edges.values():
-                ec.add(edge)
+    #         ec = EdgeCollection(nodes=nc)
+    #         for edge in data.edges.values():
+    #             ec.add(edge)
 
-            self._nodes = HigherOrderNodeCollection(nodes=nc, edges=ec)
-            # --- END ---
+    #         self._nodes = HigherOrderNodeCollection(nodes=nc, edges=ec)
+    #         # --- END ---
 
-            # get path data
-            paths = data
+    #         # get path data
+    #         paths = data
 
-            # generate first order representation of data
-            network = Network.from_paths(paths, frequencies=True)
+    #         # generate first order representation of data
+    #         network = Network.from_paths(paths, frequencies=True)
 
-            self.calculate(network, paths)
+    #         self.calculate(network, paths)
 
-        else:
-            LOG.error('A Null Model with order %s is not supported', self.order)
-            raise AttributeError
+    #     else:
+    #         LOG.error('A Null Model with order %s is not supported', self.order)
+    #         raise AttributeError
 
-    @fit.register(Network)  # type: ignore
-    def _(self, data: Network, order: Optional[int] = None) -> None:
+    # @fit.register(Network)  # type: ignore
+    # def _(self, data: Network, order: Optional[int] = None) -> None:
 
-        # Check order
-        if order is not None:
-            self._order = order
+    #     # Check order
+    #     if order is not None:
+    #         self._order = order
 
-        if 0 <= self.order <= 1:
-            super().fit(data, order=self.order)
+    #     if 0 <= self.order <= 1:
+    #         super().fit(data, order=self.order)
 
-        elif self.order > 1:
+    #     elif self.order > 1:
 
-            # TODO: create function to transfer base data from PathCollection object
-            # --- START ---
-            nc = NodeCollection()
-            for node in data.nodes.values():
-                nc.add(node)
+    #         # TODO: create function to transfer base data from PathCollection object
+    #         # --- START ---
+    #         nc = NodeCollection()
+    #         for node in data.nodes.values():
+    #             nc.add(node)
 
-            ec = EdgeCollection(nodes=nc)
-            for edge in data.edges.values():
-                ec.add(edge)
+    #         ec = EdgeCollection(nodes=nc)
+    #         for edge in data.edges.values():
+    #             ec.add(edge)
 
-            self._nodes = HigherOrderNodeCollection(nodes=nc, edges=ec)
-            # --- END ---
+    #         self._nodes = HigherOrderNodeCollection(nodes=nc, edges=ec)
+    #         # --- END ---
 
-            # get network data
-            network = data
+    #         # get network data
+    #         network = data
 
-            # generate a path representation of the data
-            paths = PathCollection(directed=network.directed,
-                                   nodes=network.nodes, edges=network.edges)
-            for edge in data.edges:
-                paths.add(edge, frequency=edge.attributes.get('frequency', 1))
+    #         # generate a path representation of the data
+    #         paths = PathCollection(directed=network.directed,
+    #                                nodes=network.nodes, edges=network.edges)
+    #         for edge in data.edges:
+    #             paths.add(edge, frequency=edge.attributes.get('frequency', 1))
 
-            self.calculate(network, paths)
+    #         self.calculate(network, paths)
 
-        else:
-            LOG.error('A Null Model with order %s is not supported', self.order)
-            raise AttributeError
+    #     else:
+    #         LOG.error('A Null Model with order %s is not supported', self.order)
+    #         raise AttributeError
 
-    def calculate(self, network: Network, paths: PathCollection) -> None:
-        """Calculate the null modell"""
+    # def calculate(self, network: Network, paths: PathCollection) -> None:
+    #     """Calculate the null modell"""
 
-        # get transition matrix of the underlying network
-        transition_matrix = network.transition_matrix(weight='frequency')
+    #     # get transition matrix of the underlying network
+    #     transition_matrix = network.transition_matrix(weight='frequency')
 
-        # generate all possible paths
-        possible_paths = self.possible_paths(paths.edges, self.order)
+    #     # generate all possible paths
+    #     possible_paths = self.possible_paths(paths.edges, self.order)
 
-        # Get all sub-paths of order-1
-        subpaths = SubPathCollection.from_paths(paths,
-                                                min_length=self.order-1,
-                                                max_length=self.order-1,
-                                                include_path=True)
+    #     # Get all sub-paths of order-1
+    #     subpaths = SubPathCollection.from_paths(paths,
+    #                                             min_length=self.order-1,
+    #                                             max_length=self.order-1,
+    #                                             include_path=True)
 
-        # add paths to the higer-order network
-        for path in possible_paths:
-            nodes: list = []
-            for subpath in self.window(path, size=self.order-1):
-                nodes.append(subpath)
+    #     # add paths to the higer-order network
+    #     for path in possible_paths:
+    #         nodes: list = []
+    #         for subpath in self.window(path, size=self.order-1):
+    #             nodes.append(subpath)
 
-            for _v, _w in zip(nodes[:-1], nodes[1:]):
+    #         for _v, _w in zip(nodes[:-1], nodes[1:]):
 
-                if _v not in self.nodes:
-                    self.nodes.add(_v)
+    #             if _v not in self.nodes:
+    #                 self.nodes.add(_v)
 
-                if _w not in self.nodes:
-                    self.nodes.add(_w)
+    #             if _w not in self.nodes:
+    #                 self.nodes.add(_w)
 
-                _nodes = (self.nodes[_v], self.nodes[_w])
+    #             _nodes = (self.nodes[_v], self.nodes[_w])
 
-                # generate the expected frequencies of all possible paths
-                if _v in subpaths:
-                    frequency = subpaths.counter[subpaths[_v]] * \
-                        transition_matrix[network.nodes.index[_w[-1].v.uid],
-                                          network.nodes.index[_w[-1].w.uid]]
+    #             # generate the expected frequencies of all possible paths
+    #             if _v in subpaths:
+    #                 frequency = subpaths.counter[subpaths[_v]] * \
+    #                     transition_matrix[network.nodes.index[_w[-1].v.uid],
+    #                                       network.nodes.index[_w[-1].w.uid]]
 
-                else:
-                    frequency = 0.0
+    #             else:
+    #                 frequency = 0.0
 
-                if _nodes not in self.edges:
-                    self.add_edge(*_nodes, possible=0,
-                                  observed=frequency, frequency=frequency)
+    #             if _nodes not in self.edges:
+    #                 self.add_edge(*_nodes, possible=0,
+    #                               observed=frequency, frequency=frequency)
 
-    def degrees_of_freedom(self, mode: str = 'path') -> int:
-        """Returns the degrees of freedom of the higher order network.
+    # def degrees_of_freedom(self, mode: str = 'path') -> int:
+    #     """Returns the degrees of freedom of the higher order network.
 
-        Since probabilities must sum to one, the effective degree of freedom is
-        one less than the number of nodes
+    #     Since probabilities must sum to one, the effective degree of freedom is
+    #     one less than the number of nodes
 
-        .. math::
+    #     .. math::
 
-           \\text{dof} = \\sum_{n \\in N} \\max(0,\\text{outdeg}(n)-1)
+    #        \\text{dof} = \\sum_{n \\in N} \\max(0,\\text{outdeg}(n)-1)
 
-        """
-        # initialize degree of freedom
-        degrees_of_freedom: int = 0
+    #     """
+    #     # initialize degree of freedom
+    #     degrees_of_freedom: int = 0
 
-        if self.order == 0:
-            degrees_of_freedom = max(0, self.number_of_nodes()-1)
+    #     if self.order == 0:
+    #         degrees_of_freedom = max(0, self.number_of_nodes()-1)
 
-        # elif mode == 'old':
-        #     # TODO : Remove this part after proper testing
-        #     A = self.network.adjacency_matrix()
+    #     # elif mode == 'old':
+    #     #     # TODO : Remove this part after proper testing
+    #     #     A = self.network.adjacency_matrix()
 
-        #     degrees_of_freedom = int(
-        #         (A ** self.order).sum()
-        #         - np.count_nonzero((A ** self.order).sum(axis=0)))
+    #     #     degrees_of_freedom = int(
+    #     #         (A ** self.order).sum()
+    #     #         - np.count_nonzero((A ** self.order).sum(axis=0)))
 
-        elif mode == 'ngram':
-            number_of_nodes = len(self.nodes.nodes)
-            degrees_of_freedom = (number_of_nodes ** self.order) * \
-                (number_of_nodes-1)
+    #     elif mode == 'ngram':
+    #         number_of_nodes = len(self.nodes.nodes)
+    #         degrees_of_freedom = (number_of_nodes ** self.order) * \
+    #             (number_of_nodes-1)
 
-        elif mode == 'path':
+    #     elif mode == 'path':
 
-            # iterate over all nodes and count outdegree
-            for outdegree in self.outdegrees().values():
-                degrees_of_freedom += max(0, int(outdegree)-1)
+    #         # iterate over all nodes and count outdegree
+    #         for outdegree in self.outdegrees().values():
+    #             degrees_of_freedom += max(0, int(outdegree)-1)
 
-        # return degree of freedom
-        return degrees_of_freedom
+    #     # return degree of freedom
+    #     return degrees_of_freedom
 
-    @staticmethod
-    def possible_paths(edges, order) -> list:
-        """Returns a dictionary of all paths with a given length
-           that can possible exists.
-        """
-        # some information for debugging
-        LOG.debug('start generate possible paths')
-        _start = datetime.datetime.now()
+    # @staticmethod
+    # def possible_paths(edges, order) -> list:
+    #     """Returns a dictionary of all paths with a given length
+    #        that can possible exists.
+    #     """
+    #     # some information for debugging
+    #     LOG.debug('start generate possible paths')
+    #     _start = datetime.datetime.now()
 
-        # start with edges, i.e. paths of length one
-        paths = [[k] for k in edges.values()]
+    #     # start with edges, i.e. paths of length one
+    #     paths = [[k] for k in edges.values()]
 
-        # extend all of those paths by an edge k-1 times
-        # TODO: speed up this function !!!
-        for _ in tqdm(range(order - 1), desc='possilbe paths'):
-            _new = list()
-            for e_1 in paths:
-                for e_2 in edges.values():
-                    if e_1[-1].w == e_2.v:
-                        _new.append(e_1 + [e_2])
-            paths = _new
+    #     # extend all of those paths by an edge k-1 times
+    #     # TODO: speed up this function !!!
+    #     for _ in tqdm(range(order - 1), desc='possilbe paths'):
+    #         _new = list()
+    #         for e_1 in paths:
+    #             for e_2 in edges.values():
+    #                 if e_1[-1].w == e_2.v:
+    #                     _new.append(e_1 + [e_2])
+    #         paths = _new
 
-        # some information for debugging
-        _end = datetime.datetime.now()
-        LOG.debug('end generate possible paths: %s seconds',
-                  (_end-_start).total_seconds())
-        return paths
+    #     # some information for debugging
+    #     _end = datetime.datetime.now()
+    #     LOG.debug('end generate possible paths: %s seconds',
+    #               (_end-_start).total_seconds())
+    #     return paths
 
-    @classmethod
-    def from_paths(cls, paths: PathCollection, **kwargs: Any):
-        """Create higher oder network from paths."""
+    # @classmethod
+    # def from_paths(cls, paths: PathCollection, **kwargs: Any):
+    #     """Create higher oder network from paths."""
 
-        order: int = kwargs.get('order', 2)
+    #     order: int = kwargs.get('order', 2)
 
-        null = cls(order=order)
-        null.fit(paths)
+    #     null = cls(order=order)
+    #     null.fit(paths)
 
-        return null
+    #     return null
 
-    @classmethod
-    def from_network(cls, network: Network, **kwargs: Any):
-        """Create higher oder network from networks."""
+    # @classmethod
+    # def from_network(cls, network: Network, **kwargs: Any):
+    #     """Create higher oder network from networks."""
 
-        order: int = kwargs.get('order', 2)
+    #     order: int = kwargs.get('order', 2)
 
-        null = cls(order=order)
-        null.fit(network)
+    #     null = cls(order=order)
+    #     null.fit(network)
 
-        return null
+    #     return null
 
 
 # =============================================================================
--- pathpy/tests/test_null_model.py
+++ pathpy/tests/test_null_model.py
@@ -3,7 +3,7 @@
 # =============================================================================
 # File      : test_null_model.py -- Test environment for null models
 # Author    : Jürgen Hackl <hackl@ifi.uzh.ch>
-# Time-stamp: <Wed 2021-03-31 11:18 juergen>
+# Time-stamp: <Fri 2021-05-28 14:42 juergen>
 #
 # Copyright (c) 2016-2019 Pathpy Developers
 # =============================================================================
@@ -26,57 +26,58 @@ def test_basic():
     # null = NullModel()
     # null.fit(paths, order=2)
 
-    null = NullModel.from_paths(paths, order=2)
+    null = NullModel(order=2)
+    # null = NullModel.from_paths(paths, order=2)
 
-    assert null.number_of_edges() == 4
-    assert null.number_of_nodes() == 4
+    # assert null.number_of_edges() == 4
+    # assert null.number_of_nodes() == 4
 
-    for e in null.edges:
-        assert e['frequency'] == 5.0
-
-
-def test_possible_paths():
-    """Test to generate all possible paths."""
-    paths = PathCollection()
-    paths.add('a', 'a', 'b', 'b', 'a')
-
-    assert len(NullModel.possible_paths(paths.edges, order=3)) == 16
+    # for e in null.edges:
+    #     assert e['frequency'] == 5.0
 
 
-def test_from_network():
+# def test_possible_paths():
+#     """Test to generate all possible paths."""
+#     paths = PathCollection()
+#     paths.add('a', 'a', 'b', 'b', 'a')
 
-    net = Network()
-    net.add_edge('a', 'c', frequency=10)
-    net.add_edge('c', 'd', frequency=10)
-    net.add_edge('b', 'c', frequency=10)
-    net.add_edge('c', 'e', frequency=10)
-
-    null = NullModel.from_network(net, order=2)
-
-    assert null.number_of_edges() == 4
-    assert null.number_of_nodes() == 4
-
-    for e in null.edges:
-        assert e['frequency'] == 5.0
+#     assert len(NullModel.possible_paths(paths.edges, order=3)) == 16
 
 
-def test_degrees_of_reedom():
-    """Tets degrees of freedom"""
-    paths = PathCollection()
-    paths.add('a', 'c', 'd', frequency=2)
-    paths.add('b', 'c', 'e', frequency=2)
+# def test_from_network():
 
-    null = NullModel.from_paths(paths, order=0)
-    assert null.degrees_of_freedom() == 4
+#     net = Network()
+#     net.add_edge('a', 'c', frequency=10)
+#     net.add_edge('c', 'd', frequency=10)
+#     net.add_edge('b', 'c', frequency=10)
+#     net.add_edge('c', 'e', frequency=10)
 
-    null = NullModel.from_paths(paths, order=1)
-    assert null.degrees_of_freedom() == 1
+#     null = NullModel.from_network(net, order=2)
 
-    null = NullModel.from_paths(paths, order=2)
-    assert null.degrees_of_freedom() == 2
+#     assert null.number_of_edges() == 4
+#     assert null.number_of_nodes() == 4
 
-    null = NullModel.from_paths(paths, order=3)
-    assert null.degrees_of_freedom() == 0
+#     for e in null.edges:
+#         assert e['frequency'] == 5.0
+
+
+# def test_degrees_of_reedom():
+#     """Tets degrees of freedom"""
+#     paths = PathCollection()
+#     paths.add('a', 'c', 'd', frequency=2)
+#     paths.add('b', 'c', 'e', frequency=2)
+
+#     null = NullModel.from_paths(paths, order=0)
+#     assert null.degrees_of_freedom() == 4
+
+#     null = NullModel.from_paths(paths, order=1)
+#     assert null.degrees_of_freedom() == 1
+
+#     null = NullModel.from_paths(paths, order=2)
+#     assert null.degrees_of_freedom() == 2
+
+#     null = NullModel.from_paths(paths, order=3)
+#     assert null.degrees_of_freedom() == 0
 
 # =============================================================================
 # eof
